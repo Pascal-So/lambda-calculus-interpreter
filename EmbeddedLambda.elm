4c27119e80db68c5df472339954a7a3eda729647
Pascal Sommer <p@pascalsommer.ch>
2017-03-09 22:26:09 +0100

    Send ace editor text to elm parser

--- EmbeddedLambda.elm
+++ EmbeddedLambda.elm
@@ -1,22 +1,23 @@
+port module EmbeddedLambda exposing (main)
+
 import Html exposing (Html, text)
 import Html.Attributes exposing (style)
 import Html.Events
-
-import Debug exposing (log)
-
 import Lambda exposing (Term)
 import LambdaLang
 
---------------------------- MAIN ------------------------------------------
+
+
+port evaluate : (String -> msg) -> Sub msg
+
 
 main =
     Html.program
         { init = init
         , update = update
         , view = view
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = \_ -> evaluate ProgramChanged
         }
-
 
 type alias Model =
     { program : String
@@ -56,20 +57,14 @@ viewStepwise : List Term -> List (Html Msg)
 viewStepwise terms =
     case terms of
         []      -> []
-        [x]     -> [Html.h3 [darkGrey, ma 5] [text <| Lambda.showTerm x]]
-        (x::xs) -> Html.h3 [lightGrey, ma 5] [text <| Lambda.showTerm x] :: viewStepwise xs
+        [x]     -> [Html.h3 [ma 5] [text <| Lambda.showTerm x]]
+        (x::xs) -> Html.h3 [ma 5] [text <| Lambda.showTerm x] :: viewStepwise xs
 
 
 view : Model -> Html Msg
 view model =
     Html.div [Html.Attributes.style [("text-align", "center")]]
-        [ Html.textarea
-            [ Html.Attributes.cols 60
-            , Html.Attributes.rows 7
-            , Html.Attributes.value model.program
-            , Html.Events.onInput ProgramChanged]
-            []
-        , case model.result of
+        [ case model.result of
             Err error ->
                 Html.h3[] [text error]
             Ok termChains ->
@@ -77,5 +72,3 @@ view model =
                 |> List.map (\x -> Html.div [mb 30] x)
                 |> Html.div []
         ]
-
-
---
+++ LambdaLang.elm
@@ -0,0 +1,268 @@
+module LambdaLang exposing
+    ( runProgram
+    )
+
+
+import Set exposing (Set)
+import Array exposing (Array)
+import Dict exposing (Dict)
+import Parser exposing ((<*), (*>), (+++), (>>=), Parser)
+import Lambda exposing (Term)
+import LambdaParser
+import Graph exposing (Graph)
+
+
+numberItems : List a -> List (Int, a)
+numberItems = List.indexedMap (\id a -> (id+1, a))
+
+sequenceResults : List (Result x a) -> Result x (List a)
+sequenceResults = List.foldr (Result.map2 (::)) (Ok [])
+
+
+runProgram : String -> Result String (List (List Term))
+runProgram str =
+  (parseProgram <* Parser.eof) str
+    -- |> log "parse"
+    |> List.head 
+    |> Maybe.map Tuple.first
+    |> Result.fromMaybe "Could not parse program"
+    |> Result.andThen (\program ->
+         if List.isEmpty program then
+           Err "Empty program" 
+         else
+           Ok program
+       )
+    |> Result.andThen (\program ->
+         if hasDuplicateVars program then
+           Err "Some variables have been assigned twice"
+         else
+           Ok program
+       )
+    |> Result.andThen (\program ->
+         let
+           graph = getDependencyGraph program
+           loops = Graph.hasLoop graph
+         in
+           if loops then
+             Err "The dependency graph contains a loop"
+           else
+             Ok graph
+       )
+    |> Result.map evaluateProgramGraph
+
+
+----------------- Dependency Graph ------------------------------
+
+indexList : List a -> List (Int, a)
+indexList = List.indexedMap (,)
+
+setFilterMap : (comparable -> Maybe comparable1) -> Set comparable -> Set comparable1
+setFilterMap f = Set.fromList << List.filterMap f << Set.toList
+
+
+getAssignmentsIdDict : List ProgramLine -> Dict Lambda.VarName Int
+getAssignmentsIdDict lst =
+  indexList lst
+  |> List.filterMap (\(id, line) ->
+       case line of 
+         Expression _ -> Nothing
+         Assignment name term -> Just (name, id)
+     )
+  |> Dict.fromList
+
+
+getDependencyGraph : List ProgramLine -> Graph ProgramLine
+getDependencyGraph lst = 
+  let
+    baseGraph = Graph.fromVerts lst
+    definitions = getAssignmentsIdDict lst
+    
+  in
+    List.map getDependencies lst
+    |> List.map (
+         setFilterMap (\dep ->
+           Dict.get dep definitions
+         )
+       )
+    |> indexList
+    |> List.foldr (\(id, deps)-> Graph.addEdges id deps) baseGraph
+
+
+
+
+
+------------- Wrapper Language Transformer ---------------
+
+getDependencies : ProgramLine -> Set Lambda.VarName
+getDependencies line =
+    case line of
+        Expression term -> Lambda.getFreeVars term
+        Assignment _ term -> Lambda.getFreeVars term
+
+
+getAssignmentsDict : List ProgramLine -> Dict Lambda.VarName Term
+getAssignmentsDict lines =
+    lines
+    |> List.filterMap (\line ->
+         case line of
+            Assignment a b -> Just (a, b)
+            Expression _   -> Nothing
+       )
+    |> Dict.fromList
+
+
+getExpressionTerms : List ProgramLine -> List Term
+getExpressionTerms lines =
+    lines
+    |> List.filterMap (\line ->
+         case line of
+             Expression term -> Just term
+             Assignment _ _  -> Nothing
+       )
+
+
+wrapInLambdas : Dict Lambda.VarName Term -> Term -> Term
+wrapInLambdas dict term =
+    let
+        freeVars = Lambda.getFreeVars term
+        termsToAssign = dict
+        |> Dict.filter (\var _ ->
+            Set.member var freeVars
+           )
+        |> Dict.toList
+    in
+        termsToAssign
+        |> List.foldr (\(name, val) accTerm ->
+            Lambda.App (Lambda.Lambda name accTerm) val
+         ) term
+
+
+-- only run on valid program (no duplicate variable assignments),
+-- otherwise duplicate assignments will be overridden
+programToExpressions : List ProgramLine -> List Term
+programToExpressions lines =
+    let
+        assignments = getAssignmentsDict lines
+        terms = getExpressionTerms lines
+    in
+        List.map (wrapInLambdas assignments) terms
+
+
+hasDuplicateVars : List ProgramLine -> Bool
+hasDuplicateVars prog =
+    let
+        vars =
+            prog
+            |> List.filterMap (\line ->
+                case line of
+                    Assignment var _ -> Just var
+                    Expression _     -> Nothing
+               )
+        len = List.length vars
+        uniqueLen = Set.size (Set.fromList vars)
+    in
+        len /= uniqueLen
+
+flattenMaybe : Maybe (Maybe a) -> Maybe a
+flattenMaybe m =
+    case m of
+        Nothing -> Nothing
+        Just a  -> a
+
+
+getTerm : Int -> Graph ProgramLine -> Maybe Term
+getTerm id graph =
+    Graph.getValue id graph
+    |> Maybe.map (\val ->
+           case val of
+               Assignment _ term -> term
+               Expression term   -> term
+       )
+
+getDependencyName : Int -> Graph ProgramLine -> Maybe Lambda.VarName
+getDependencyName id graph =
+    Graph.getValue id graph
+    |> Maybe.andThen (\val ->
+           case val of
+               Assignment name _ -> Just name
+               Expression _      -> Nothing
+       )
+
+transposeTupleMaybe : (Maybe a, Maybe b) -> Maybe (a, b)
+transposeTupleMaybe pair =
+    case pair of
+        (Just a, Just b) -> Just (a, b)
+        _                -> Nothing
+
+last : List a -> Maybe a
+last = List.head << List.reverse
+
+-- is already cycle free
+evaluateProgramGraph : Graph ProgramLine -> List (List Term)
+evaluateProgramGraph graph = 
+    let
+        evalOrder = Graph.topoSort graph
+            |> List.reverse
+        size = Graph.getSize graph
+        evaluated = Array.repeat size Nothing
+    in
+        List.foldl (\id evaluated ->
+            let
+                deps = Graph.getOutEdges id graph
+                    |> Maybe.withDefault Set.empty
+                    |> Set.toList
+                    |> List.map (\id -> 
+                           Array.get id evaluated
+                           |> flattenMaybe
+                           |> Maybe.andThen last
+                           |> (\val -> (getDependencyName id graph, val))
+                           |> transposeTupleMaybe
+                       )
+                    |> List.filterMap identity
+                    |> Dict.fromList
+                evaluatedTerm = getTerm id graph
+                    |> Maybe.map (Lambda.evaluateStepwise << wrapInLambdas deps)
+            in
+                Array.set id evaluatedTerm evaluated
+        ) evaluated evalOrder
+        |> Array.toList
+        |> List.filterMap identity
+  
+
+------------ Wrapper Language Parser ----------------------
+
+
+type ProgramLine = Assignment Lambda.VarName Term | Expression Term
+
+
+parseAssignment : Parser ProgramLine
+parseAssignment =
+    LambdaParser.parseVarName >>= \varname ->
+    Parser.whitespace *>
+    Parser.char '=' *>
+    Parser.whitespace *>
+    Parser.inRestOfLine LambdaParser.parseTerm >>= \term ->
+    Parser.return (Assignment varname term)
+
+parseExpression : Parser ProgramLine
+parseExpression =
+    LambdaParser.parseTerm >>= \term ->
+    Parser.return (Expression term)
+
+
+parseProgramLine : Parser ProgramLine
+parseProgramLine =
+    Parser.whitespace *>
+    parseAssignment +++ parseExpression >>= \line ->
+    Parser.whitespace *>
+    Parser.return line
+
+
+
+parseProgram : Parser (List ProgramLine)
+parseProgram =
+    Parser.many
+        ( Parser.whitespace *>
+          (Parser.inRestOfLine (parseExpression <* Parser.whitespace) +++ parseAssignment)
+          <* Parser.whitespace) >>= \lines ->
+    Parser.return lines
